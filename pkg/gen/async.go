d64678651ee4d01a72d2b3638bf1f3614765360f
Danilo Ramirez <daramir9999@gmail.com>
2020-10-18 01:25:08 +1100

    delete unused files

--- pkg/gen/async.go
+++
@@ -1,3 +0,0 @@
-package gen
-
-//go:generate sh -c "rm -f ./async/*; go run github.com/keep-network/keep-common/tools/generators/promise/ -d ./async *event.EntrySubmitted *event.GroupTicketSubmission *event.GroupRegistration *event.Request *event.DKGResultSubmission *event.EntryGenerated"
